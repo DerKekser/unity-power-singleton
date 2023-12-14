3212542b3364006e18d08a0775ae4cb31d111271
DerKekser <benjamin.buss@gmx.de>
2023-12-14 18:19:55 +0100

    Added unity object null check

--- Assets/Kekser/PowerSingleton/Single.cs
+++ Assets/Kekser/PowerSingleton/Single.cs
@@ -4,8 +4,17 @@ namespace Kekser.PowerSingleton
 {
     public static class Single<T> where T : class
     {
-        private static T CachedInstance { get; set; }
+        private static Object _rawInstance;
+        private static T _instance { get; set; }
 
-        public static T Instance => CachedInstance ??= PowerSingletonManager.Get(typeof(T)) as T;
+        public static T Instance {
+            get
+            {
+                if (_rawInstance != null) return _instance;
+                _rawInstance = PowerSingletonManager.Get(typeof(T));
+                _instance = _rawInstance as T;
+                return _instance;
+            }
+        }
     }
 }
