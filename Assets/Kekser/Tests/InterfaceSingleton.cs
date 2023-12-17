4b15977f303fdcd8edab275cca6a645e28b6d12f
Benjamin Buß <benjamin.buss@gmx.de>
2023-12-17 13:02:42 +0100

    Added testing

---
+++ Assets/Kekser/Tests/InterfaceSingleton.cs
@@ -0,0 +1,21 @@
+﻿using Kekser.PowerSingleton.Attributes;
+using UnityEngine;
+
+namespace Kekser.Tests
+{
+    [PowerSingleton(typeof(IInterfaceSingleton))]
+    public class InterfaceSingleton : MonoBehaviour, IInterfaceSingleton
+    {
+        public static bool Created = false;
+        
+        private void Awake()
+        {
+            Created = true;
+        }
+        
+        private void OnDestroy()
+        {
+            Created = false;
+        }
+    }
+}
