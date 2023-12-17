9d3337079f6d105d34fa6677fc831871af191ba1
Benjamin Buß <benjamin.buss@gmx.de>
2023-12-17 18:10:31 +0100

    Added manual binding

---
+++ Assets/Kekser/Tests/BindSingleton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Kekser.Tests
+{
+    public class BindSingleton : MonoBehaviour
+    {
+        public static bool Created = false;
+        
+        private void Awake()
+        {
+            Created = true;
+        }
+        
+        private void OnDestroy()
+        {
+            Created = false;
+        }
+    }
+}
